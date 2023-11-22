287e2fd883cf8ef2c5b4a6a80ac52f115498c117
leo <fserz1210@gmail.com>
2023-11-22 11:21:14 +0800

    update .gitignore

---
+++ rabbit-delay2-PROBLEM/src/main/java/com/rabbitdelay2/RabbitTTLApplication2.java
@@ -0,0 +1,24 @@
+package com.rabbitdelay2;
+
+
+import com.rabbitdelay2.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitTTLApplication2 implements ApplicationRunner {
+    @Autowired
+    private MessageService messageService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitTTLApplication2.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        messageService.sendMsg();
+    }
+}
