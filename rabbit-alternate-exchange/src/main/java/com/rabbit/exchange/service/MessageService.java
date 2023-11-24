2d135950ced0ed0b6cc05dfa6757d5d28bc3f7f1
leo <fserz1210@gmail.com>
2023-11-24 15:20:23 +0800

    exchange autoDelete durable(false)  and alternate exchange

--- rabbit-alternate-exchange/src/main/java/com/rabbit/exchange/service/MessageService.java
+++ rabbit-alternate-exchange/src/main/java/com/rabbit/exchange/service/MessageService.java
@@ -3,7 +3,6 @@ package com.rabbit.exchange.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageBuilder;
-import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
